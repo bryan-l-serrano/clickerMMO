d32c2cec1053c37d23b99d2c6ff382f5790a072d
bryan-l-serrano <bryan.l.serrano@gmail.com>
2022-05-01 16:22:27 -0400

    added generate id and basic storage functions

--- App.tsx
+++ App.tsx
@@ -1,5 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import uuid from 'react-native-uuid'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+
+const generateId = () =>{
+  let id = uuid.v4();
+  let idString = id.toString();
+  return idString;
+}
+
+const storeClicks =  async (userClicks:number) =>{
+  try{
+    AsyncStorage.setItem("@clicks", userClicks.toString());
+  }
+  catch(e){
+    console.log("failed to store clicks");
+  }
+}
+
+const storeId = async (id:string) =>{
+  try{
+    AsyncStorage.setItem("@id", id);
+  }
+  catch(e){
+    console.log("failed to store id");
+  }
+}
+
+
+
 
 export default function App() {
   return (
