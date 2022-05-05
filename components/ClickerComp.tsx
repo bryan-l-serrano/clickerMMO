637a8033c4358d5e4fc23c97ef7428b23eca4382
shigal <shigalhammoud@gmail.com>
2022-05-05 18:28:26 -0400

    added button and text

--- components/ClickerComp.tsx
+++ components/ClickerComp.tsx
@@ -1,111 +1,131 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import uuid from 'react-native-uuid'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-const generateId = () =>{
-    let id = uuid.v4();
-    let idString = id.toString();
-    return idString;
+const generateId = () => {
+  let id = uuid.v4();
+  let idString = id.toString();
+  return idString;
+}
+
+const storeClicks = async (userClicks: number) => {
+  try {
+    AsyncStorage.setItem("@clicks", userClicks.toString());
   }
-  
-  const storeClicks =  async (userClicks:number) =>{
-    try{
-      AsyncStorage.setItem("@clicks", userClicks.toString());
-    }
-    catch(e){
-      console.log("failed to store clicks");
-    }
+  catch (e) {
+    console.log("failed to store clicks");
   }
-  
-  const storeId = async (id:string) =>{
-    try{
-      AsyncStorage.setItem("@id", id);
-    }
-    catch(e){
-      console.log("failed to store id");
-    }
+}
+
+const storeId = async (id: string) => {
+  try {
+    AsyncStorage.setItem("@id", id);
   }
-  
-  const getClicks = async () =>{
-    try{
-      const userClicks = await AsyncStorage.getItem("@clicks");
-      return userClicks != null ? parseInt(userClicks) : 0;
-    }
-    catch(e){
-      console.log("failed to load clicks")
-      return 0;
-    }
+  catch (e) {
+    console.log("failed to store id");
   }
-  
-  const getId = async () =>{
-    try{
-      const id = await AsyncStorage.getItem("@id");
-      return id != null ? id : "";
-    }
-    catch(e){
-      console.log("failed to getId");
-      return "";
-    }
+}
+
+const getClicks = async () => {
+  try {
+    const userClicks = await AsyncStorage.getItem("@clicks");
+    return userClicks != null ? parseInt(userClicks) : 0;
   }
+  catch (e) {
+    console.log("failed to load clicks")
+    return 0;
+  }
+}
+
+const getId = async () => {
+  try {
+    const id = await AsyncStorage.getItem("@id");
+    return id != null ? id : "";
+  }
+  catch (e) {
+    console.log("failed to getId");
+    return "";
+  }
+}
 
 type ClickerCompProps = {
 
 };
 
 type ClickerCompState = {
-    globalClicks:number;
-    uuid:string;
-    clicks:number;
+  globalClicks: number;
+  uuid: string;
+  clicks: number;
+  rank: number;
 }
 
-  class ClickerComp extends Component<ClickerCompProps, ClickerCompState>{
-    constructor(props: ClickerCompProps){
-        super(props);
-        this.state = {
-            globalClicks:0,
-            uuid:"",
-            clicks:0
+class ClickerComp extends Component<ClickerCompProps, ClickerCompState>{
+  constructor(props: ClickerCompProps) {
+    super(props);
+    this.state = {
+      globalClicks: 0,
+      uuid: "",
+      clicks: 0,
+      rank: 0
 
-        }
-    };
-
-    componentDidMount(){
-        getId().then((userid) =>{
-            if(userid){
-                this.setState({uuid:userid})
-            }
-            else{
-                let userid = generateId();
-                storeId(userid);
-                this.setState({uuid:userid});
-            }
-        });
-
-        getClicks().then((userclicks) =>{
-            this.setState({clicks:userclicks})
-        })
     }
+  };
 
+  componentDidMount() {
+    getId().then((userid) => {
+      if (userid) {
+        this.setState({ uuid: userid })
+      }
+      else {
+        let userid = generateId();
+        storeId(userid);
+        this.setState({ uuid: userid });
+      }
+    });
 
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text>userid: {this.state.uuid}</Text>
-            </View>
-        )
-    }
+    getClicks().then((userclicks) => {
+      this.setState({ clicks: userclicks })
+    })
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+onSingleClick() {
+  this.setState({clicks: this.state.clicks + 1});
+  storeClicks(this.state.clicks);
+}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>userid: {this.state.uuid}</Text>
+        <Text>Global Count: {this.state.globalClicks}</Text>
+        <Text>Rank: {this.state.rank}</Text>
+        <Text>Local Count: {this.state.clicks}</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={()=> this.onSingleClick()}
+        >
+          <Text>Press Here</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  }
+});
 
 
-  export default ClickerComp;
+export default ClickerComp;
